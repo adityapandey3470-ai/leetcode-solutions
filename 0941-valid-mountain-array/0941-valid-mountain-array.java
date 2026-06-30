f5db3c9b6cb84d872954d7c168edcacab61ebaf3
Aditya Chaturvedi <adityapandey3470@gmail.com>
2026-06-30 23:28:40 +0530

    Time: 2 ms (84.24%), Space: 47.4 MB (72.46%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
 
-        
+        if(arr.length < 3){
+            return false;
+        }
           int i = 0;
           while(i < arr.length - 1 && arr[i] < arr[i + 1]){
 
