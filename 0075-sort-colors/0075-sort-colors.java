dcf40b00a57ad7e98d24387b580424023197f625
Aditya Chaturvedi <adityapandey3470@gmail.com>
2026-07-03 00:00:09 +0530

    Time: 97 ms (2.83%), Space: 43.6 MB (24.54%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int temp = 0;
+        for(int i = 0; i < nums.length-1; i++){
+            for(int j = 0; j < nums.length-i-1; j++){
+
+                if(nums[j] > nums[j + 1]){
+
+                    temp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = temp;
+
+
+                }
+
+            }
+
+           
+        }
+        
+    }
+}
