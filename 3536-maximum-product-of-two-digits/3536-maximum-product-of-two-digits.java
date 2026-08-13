53d6f70dbf9a862d98dc57672d1cd791311f2b5d
Aditya Chaturvedi <adityapandey3470@gmail.com>
2026-08-13 17:58:35 +0530

    Time: 1 ms (100%), Space: 42.9 MB (8.33%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -14,10 +14,6 @@ class Solution {
              } else {
                 secondLargest = Math.max(secondLargest, digit);
              }
-
-
-
-       
         }
        return largest * secondLargest;
     }
