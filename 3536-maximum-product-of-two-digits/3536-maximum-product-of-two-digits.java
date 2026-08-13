56892ec29771b415f26f2eef57a974d5d2f657cc
Aditya Chaturvedi <adityapandey3470@gmail.com>
2026-08-13 17:57:42 +0530

    Time: 1 ms (100%), Space: 42.8 MB (35.61%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int n) {
+
+        int largest = 0;
+        int secondLargest = 0;
+
+        while(n > 0) {
+            int digit = n % 10;
+             n = n / 10;
+
+             if(digit > largest){
+                secondLargest = largest;
+                largest = digit;
+             } else {
+                secondLargest = Math.max(secondLargest, digit);
+             }
+
+
+
+       
+        }
+       return largest * secondLargest;
+    }
+}
