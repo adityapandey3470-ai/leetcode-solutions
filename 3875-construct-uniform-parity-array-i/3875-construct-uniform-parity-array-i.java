6e5ba623db15e0efe5269a27edd903cb461f3294
Aditya Chaturvedi <adityapandey3470@gmail.com>
2026-09-04 21:46:29 +0530

    Time: 0 ms (100%), Space: 44.9 MB (78.62%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+       for(int i = 0; i <nums1.length; i++){
+
+        if(nums1[i] % 2 == 0 || nums1[i] % 2 != 0){
+            return true;
+        }
+
+       }
+        return false;
+        
+    }
+}
